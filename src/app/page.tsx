f8ddef17c3b90393be33e61fa5939487900ad8bb
Salim Rutaganda <rutagandasalim@gmail.com>
2025-02-02 19:38:19 +0200

    feat: Add MakerIntro component to showcase benefits of ShipFree for founders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import CTA from "@/components/Cta";
 import FAQ from "@/components/Faq";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/Hero";
+import MakerIntro from "@/components/MakerIntro";
 import Navbar from "@/components/Navbar";
 import PricingSection from "@/components/pricing";
 import TestimonialsPage from "@/components/Testimonials";
@@ -12,6 +13,7 @@ export default function Home() {
     <div className="bg-[#212121]">
       <Navbar />
       <HeroSection />
+      <MakerIntro />
       <PricingSection />
       <FAQ />
       <TestimonialsPage />
